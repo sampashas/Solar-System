561a6b8f2d522bfebdd75c8220ffcf4f6a45c695
Pavel S <sampashass@gmail.com>
2023-02-15 02:19:35 +0700

    ok

--- components/Scene.js
+++ components/Scene.js
@@ -4,15 +4,11 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 function Scene() {
   const canvasRef = useRef(null)
-  const [sphere, setSphere] = useState(null)
-  const [controls, setControls] = useState(null)
 
   useEffect(() => {
-    // сцена
+    // SETTINGS
     const scene = new THREE.Scene()
-    scene.background = new THREE.Color(0x000000)
-
-    // камера
+    scene.background = new THREE.Color(0x181818)
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
@@ -20,44 +16,71 @@ function Scene() {
       1000,
     )
     camera.position.z = 5
-
-    // как рендерится
     const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current })
     renderer.setSize(window.innerWidth, window.innerHeight)
-
-    // загрузчик текстур
-    const textureLoader = new THREE.TextureLoader()
-    const texture = textureLoader.load('ava.png')
-
-    // элементы сцены
-    const sphereGeometry = new THREE.SphereGeometry(1, 32, 32)
-    const sphereMaterial = new THREE.MeshLambertMaterial({ map: texture })
-    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-    scene.add(sphere)
-
-    // свет
-    scene.add(new THREE.AmbientLight(0x222222))
-
-    const light = new THREE.DirectionalLight(0xffffff, 1)
-    light.position.set(20, 20, 0)
-    scene.add(light)
-
-    // хелпер
+    // Helper
     scene.add(new THREE.AxesHelper(20))
 
-    // контролы
+    // MOVING
     const controls = new OrbitControls(camera, renderer.domElement)
     controls.enableDamping = true
-    controls.dampingFactor = 0.05
 
-    // анимация
-    const animate = () => {
-      requestAnimationFrame(animate)
-      sphere.rotation.y += 0.01
-      controls.update()
-      renderer.render(scene, camera)
+    // LIGHT
+    // 1
+    var light2 = new THREE.DirectionalLight(0xffffff, 1)
+    light2.position.set(20, 20, 200)
+    scene.add(light2)
+    // 2
+    const light = new THREE.PointLight(0xffffff, 1, 1000)
+    light.position.set(20, 200, 20)
+    scene.add(light)
+
+    // LAYERS
+    const geometry = new THREE.SphereGeometry(64, 32, 32)
+    const uniforms = {
+      time: { type: 'f', value: 1.0 },
+      resolution: { type: 'v2', value: new THREE.Vector2() },
     }
-    animate()
+
+    uniforms.resolution.value.x = 500
+    uniforms.resolution.value.y = 500
+
+    const material = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: `
+        uniform vec2 resolution;
+        uniform float time;
+        varying vec2 vUv;
+        void main() {
+          vUv = uv;
+          vec2 pos = position.xy / resolution.xy;
+          pos.y += time * 0.05;
+          vec4 modelViewPosition = modelViewMatrix * vec4(pos, 0.0, 1.0);
+          gl_Position = projectionMatrix * modelViewPosition;
+        }
+      `,
+      fragmentShader: `
+        uniform vec2 resolution;
+        uniform float time;
+        varying vec2 vUv;
+        void main() {
+          vec2 pos = vUv;
+          pos.y += (sin((pos.x + (time * 0.5)) * 10.0) * 0.1) + (sin((pos.x + (time * 0.5)) * 32.0) * 0.01);
+          gl_FragColor = vec4(0.2, 0.4, 0.6, 1.0) * vec4(pos.y);
+        }
+      `,
+    })
+
+    // GET
+    const sphere = new THREE.Mesh(geometry, material)
+    scene.add(sphere)
+
+    const render = () => {
+      renderer.render(scene, camera)
+      requestAnimationFrame(render)
+    }
+
+    render()
   }, [])
 
   return <canvas ref={canvasRef} />
